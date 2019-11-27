dc0450c00df4e33194164f798ed0efaf92a12a7e
Cloud User <ec2-user@ip-172-31-14-223.ap-south-1.compute.internal>
2019-11-27 09:03:13 +0000

    Cube operation Added

--- first.sh
+++ first.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo "dheeraj"
+echo "enter any positive number"
+read var
+echo "squre of $var" 
+echo "$(( $var * $var ))"
+echo "cube of $var"
+echo "$(($var * $var * $var))"
+#echo "dheeraj"r 
